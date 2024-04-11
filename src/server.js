f6b8e7dcc6d8b722b348e011486e293a8c229b90
winati17 <winatimn@gmail.com>
2024-04-12 00:40:23 +0800

    module web service with node.js by dicoding

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 3000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        "origin": ["*"],
+        "headers": ["Accept", "Authorization", "Content-Type", "If-None-Match", "Accept-language"],
+        "additionalHeaders": ["X-Requested-With"]
+      },
+    }
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
+
